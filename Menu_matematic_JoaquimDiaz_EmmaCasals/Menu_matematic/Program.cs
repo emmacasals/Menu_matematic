7936f7714231b0036c6147abf30d9819b9bcb050
a2308754 <ecasalsicordones@gmail.com>
2024-01-17 13:47:57 +0100

    inici projecte

---
+++ Menu_matematic_JoaquimDiaz_EmmaCasals/Menu_matematic/Program.cs
@@ -0,0 +1,141 @@
+﻿/*1.- Menú matemàtic
+Fer un menú amb 9 opcions (8 + q per sortir). Cada opció del menú cridarà a una acció Maxim,
+Mcd, Mcm, Factorial, Combinatori, MostrarDivisorMajor, EsPrimer, NPrimersPrimers que
+demanarà les entrades validades, farà els càlculs, i mostrarà el resultat. Les entrades hauran de
+ser validades per algunes de les funcions de validació. Cal presentar un projecte únic amb totes
+les funcions i accions proposades a més a més del programa principal que implementa el
+menú.*/
+
+
+//MAIN:Fem la crida de les altres funcions 
+static void Main(string[] args)
+{
+    int R = 1;
+    do
+    Menu(R);
+    while (R != 0);
+}
+
+static int Menu(int R)
+{
+    R = Convert.ToInt32(Console.ReadLine());
+    //Crear un menu. Et te que tornar el numero que has posat en el menu per saber quan el programa pot parar (quan poses 0).
+    if (R == 0)
+    return R;
+
+    Console.Clear();
+    Console.WriteLine("MENÚ D'OPCIONS MATEMÀTIQUES");
+    Console.WriteLine("\n" +
+        "\n 1.-MAXIM" +
+        "\n 2.-MCD" +
+        "\n 3.-MCM" +
+        "\n 4.-FACTORIAL" +
+        "\n 5.-COMBINATORI" +
+        "\n 6.-MOSTRAR DIVISOR MAJOR" +
+        "\n 7.-ES PRIMER" +
+        "\n 8.-NPRIMERS PRIMERS" +
+        "\n 9.-SORTIDA \n");
+    Console.WriteLine("Selecciona la opció que desitja: ");
+   R=Convert.ToInt32(Console.ReadLine());
+
+    switch (R)
+    {
+        case 1:
+            Maxim();
+            break;
+        case 2:
+            MCD();
+            break;
+        case 3:
+            MCM();
+            break;
+        case 4:
+            Factorial();
+            break;
+        case 5:
+            Combinatori();
+            break; 
+        case 6:
+            MostrarDivisorMajor();
+            break;
+        case 7:
+            EsPrimer();
+            break;
+        case 8:
+            NPrimersPrimers();
+            break;
+        case 9:
+            Console.WriteLine("Estas sortint del menú d'operacions");
+            break;
+        default:
+            Console.WriteLine("Introdueix una opció vàlida,sisplau");
+            break;
+    }
+    Console.ReadKey();  
+}
+
+//Maxim
+static int Maxim(int num)
+{
+  
+}
+//Mcd
+static int MCD(int num)
+{
+}
+//Mcm
+static int MCM(int num)
+{
+}
+//WEmmackvfvn
+//Factorial
+//Aquesta es la més correcta
+static int Factorial(int num)
+{
+
+    int res = 1;
+    for (int i = 1; i <= num; i++)
+        res *= i;
+    return res;
+}
+/*
+static double Factorial(double num)
+{
+
+    int res = 1;
+    for (int i = 1; i <= num; i++)
+        res *= i;
+    return res;
+}
+*/
+
+
+//Combinatori
+//COMBINATORI FACTORIAL
+static double Combinatori(int n, int m)
+{
+    double resultat;
+    resultat = Factorial(n) / (Factorial(m)) / Factorial(n - m);
+    return resultat;
+}
+//MostrarDivisorMajor
+static int MostrarDivisorMajor(int num)
+{
+}
+//EsPrimer
+static int EsPrimer(int num)
+{
+}
+//NPrimersPrimers
+static double NPrimersPrimers(int n, int m)
+{
+}
+
+static char Sortida (char q)
+{
+    q= Convert.ToChar(Console.ReadLine);
+    if (q == q)
+    {
+        Menu();
+    }
+}
